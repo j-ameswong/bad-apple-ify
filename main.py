73fb585c6c660794b0c6d5a07337a2da6a56e41e
Cheng An Wong <wongchengan@gmail.com>
2026-02-14 17:08:55 +0000

    feat: move write_frames out of extract_frames
    Also refactored source > src and save src_dimensions to config

--- main.py
+++ main.py
@@ -13,41 +13,58 @@ class Config:
             (16, 10)
     )
 
-    source_fps: int = 30
+    input_dir: str
+    output_dir: str
+    src_fps: int = 30
+    src_dimensions: tuple = (0, 0)
     output_fps: int = 30
     aspect_ratio: tuple = ASPECT_RATIOS[0]
-    save_source: bool = False
     img_format: Literal["png", "jpg"] = "png"
     grid_size: int = 16
+
+    def src_ratio(self) -> float:
+        return self.src_dimensions[0] / float(self.src_dimensions[1])
 
     def __post_init__(self):
         if self.aspect_ratio not in self.ASPECT_RATIOS:
             raise ValueError(f"{self.aspect_ratio} is not a valid aspect ratio, "
                 " please select from {self.ASPECT_RATIOS}")
 
-def extract_frames(config: Config, input_dir: str, output_dir: str) -> np.ndarray:
-    """Extract and convert all frames to grayscale"""
-    if config.save_source:
-        output = Path(output_dir)
-        output.mkdir(parents=True, exist_ok=True)
+def write_frames(frames: np.ndarray, config: Config):
+    output = Path(config.output_dir)
+    output.mkdir(parents=True, exist_ok=True)
 
-    cap = cv2.VideoCapture(input_dir)
+    for f in tqdm.tqdm(range(len(frames)),
+            desc = f"Writing frames to {output}..."):
+
+        to_write = cv2.resize((frames[f] * 255.0).astype(np.uint8),
+                              (config.src_dimensions[0], config.src_dimensions[1]),
+                              interpolation=cv2.INTER_NEAREST)
+
+        cv2.imwrite(f"{output}/frame_{f:05d}.{config.img_format}", to_write)
+
+    print(f"Success! Images extracted, {len(frames)} frames processed")
+
+def extract_frames(config: Config) -> np.ndarray:
+    """Extract and convert all frames to grayscale"""
+    cap = cv2.VideoCapture(config.input_dir)
     if not cap.isOpened():
-        raise ValueError(f"Video at {input_dir} not found!")
+        raise ValueError(f"Video at {config.input_dir} not found!")
 
     # Save frame rate
-    config.source_fps = round(cap.get(cv2.CAP_PROP_FPS))
-    config.output_fps = config.source_fps
+    config.src_fps = round(cap.get(cv2.CAP_PROP_FPS))
+    config.output_fps = config.src_fps
 
     # Calculate nearest Config.ASPECT_RATIOS
-    source_ratio = [int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))]
-    diffs = [abs((source_ratio[0] / source_ratio[1])
-                 - (ratio[0] / float(ratio[1])))
+    config.src_dimensions = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    diffs = [abs(config.src_ratio() - (ratio[0] / float(ratio[1])))
              for ratio in config.ASPECT_RATIOS] 
 
     config.aspect_ratio = config.ASPECT_RATIOS[diffs.index(min(diffs))]
 
-    # grid here is the new pixelised array, higher res by increasing grid_size
+    # grid here is the new pixelated image vector, higher res by increasing grid_size
     grid = tuple(x * config.grid_size for x in config.aspect_ratio)
     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -59,14 +76,9 @@ def extract_frames(config: Config, input_dir: str, output_dir: str) -> np.ndarra
         ret, frame = cap.read()
 
         output_data[f] = fingerprint(frame, grid)
-        if config.save_source:
-            to_write = cv2.resize((output_data[f] * 255.0).astype(np.uint8),
-                                  (source_ratio[0], source_ratio[1]),
-                                  interpolation=cv2.INTER_NEAREST)
-
-            cv2.imwrite(f"{output}/frame_{f:05d}.{config.img_format}", to_write)
 
     cap.release()
+    print(f"Success! Images extracted, {num_frames} frames processed")
     return output_data
 
 def fingerprint(img: np.ndarray, grid: tuple) -> np.ndarray:
@@ -91,12 +103,10 @@ def get_stretched_res(img: np.ndarray, config: Config) -> tuple:
     return (y, x)
 
 def main():
-    config = Config(save_source=True)
+    config = Config(input_dir="./assets/bad_apple.mp4", output_dir="./output/")
 
-    frames = extract_frames(config, "./assets/bad_apple.mp4", "./output")
-    print(f"Success! Images extracted, {len(frames)} frames processed")
-
-    found = False
+    frames = extract_frames(config)
+    write_frames(frames, config)
 
 if __name__ == "__main__":
     main()
